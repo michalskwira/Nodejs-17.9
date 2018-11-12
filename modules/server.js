98176430df38c345a22f5c6bcec36a93d81fdeb5
Michal Gacek <michal.gacek@first-utility.com>
2018-11-12 18:38:26 +0100

    Fixes

--- modules/server.js
+++ modules/server.js
@@ -9,18 +9,17 @@ function start() {
     console.log("Zapytanie " + request.url + " odebrane.");
 
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-    console.log(handlers)
-    switch (responsquest.url) {
-	case '/':
-	case '/start':
-    	handlers.welcome(request, response);
-    	break;
-	case '/upload':
-    	handlers.upload(request, response);
-    	break;
-	case '/show':
-    	handlers.show(request, response);
-   		break;
+    switch (request.url) {
+	    case '/':
+	    case '/start':
+    	  handlers.welcome(request, response);
+    	  break;
+	    case '/upload':
+    	  handlers.upload(request, response);
+    	  break;
+	    case '/show':
+    	  handlers.show(request, response);
+   		  break;
     }
   }
 
